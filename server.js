7d9373453f1b29b10f7c99eaa4f88cdcdff808a9
bim <alamsyahbima22@gmail.com>
2024-02-01 15:52:31 +0700

    remove comments

--- server.js
+++ server.js
@@ -1,20 +1,8 @@
-const firebase = require("firebase/app");
-require("firebase/auth");
 const express = require("express");
 const cors = require("cors");
 const http = require("http");
-const socketIO = require("socket.io");
 const mongoose = require("mongoose");
-const jwtMiddleware = require("./app/middlewares/jsMiddleware");
-const db = require("./app/models");
-const matchRoutes = require("./app/routes/homeTeam.route");
-const multer = require("multer");
 require("dotenv").config();
-
-// Middleware for handling file uploads
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
-const router = express.Router();
 
 const corsOptions = {
   origin: "*",
@@ -27,13 +15,6 @@ app.use(express.json({ limit: "50mb" }));
 
 // Create an HTTP server using the Express app
 const server = http.createServer(app);
-
-// Create a Socket.IO instance attached to the server
-const io = require("socket.io")(server, {
-  cors: {
-    origin: "*",
-  },
-});
 
 // Connect to the database
 const mongooseConfig = {
@@ -49,7 +30,7 @@ mongoose
     process.exit();
   });
 
-// Include your existing routes here
+// Include existing routes
 require("./app/routes/mahasiswa.routes")(app);
 require("./app/routes/coach.route")(app);
 require("./app/routes/homeTeam.route")(app);
@@ -58,19 +39,11 @@ require("./app/routes/team.route")(app);
 require("./app/routes/score.route")(app);
 require("./app/routes/playerHome.route")(app);
 require("./app/routes/playerAway.route")(app);
-// app.use("/auth", require("./app/routes/auth.route"));
-// app.use("/protected", jwtMiddleware);
-// router.get("/protected/data", jwtMiddleware, (req, res) => {
-// Access the protected data using req.user
-//   res.json({ message: "Protected data accessed successfully", user: req.user });
-// });
-
-// Routes
 
 const pictureRoutes = require("./app/routes/picture.route");
 app.use("/pictures", pictureRoutes);
 
-const IP_ADDRESS = process.env.IP || "localhost"; // Replace with the desired IP address
+const IP_ADDRESS = process.env.IP || "localhost";
 const PORT = process.env.PORT || 8000;
 server.listen(PORT, IP_ADDRESS, () => {
   console.log(`Server running on http://${IP_ADDRESS}:${PORT}`);
